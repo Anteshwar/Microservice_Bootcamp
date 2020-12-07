4bb11d166e757888811be39ed3b7473eaa35e281
Anteshwar Tondare <tondareanteshwar@gmail.com>
2020-12-07 13:08:16 +0530

    Add files via upload
    Api Gateway commit

---
+++ lab6_api_gateway/src/main/java/com/bootcamp/gateway/Lab6ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.bootcamp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO LABA-1.1 setup pom with dependencies and configure the gateway Setup
+ * configuration server config file from config/apigateway-dev.yaml
+ * 
+ * @author amit
+ *
+ */
+@SpringBootApplication
+public class Lab6ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab6ApiGatewayApplication.class, args);
+	}
+
+}
